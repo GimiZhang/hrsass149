9542374cff46ceb5c1ddefc4a3081802ae0400ef
zwm <608564@hopson.com.cn>
2023-07-04 16:30:13 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: 'hrsass-149',
+
+  /**
+   * @type {boolean} true | false
+   * @description 设置头部固定
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description 设置左侧导航栏 头部是否显示
+   */
+  sidebarLogo: false
+}
